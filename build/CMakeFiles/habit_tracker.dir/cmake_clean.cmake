cc6569087d0a74d2152274cc50e5eb89a426194c
Siomayeater <jaydenwongkar1832@gmail.com>
2025-10-05 13:52:34 -0400

    Initial commit of Habit Tracker project

---
+++ build/CMakeFiles/habit_tracker.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/habit_tracker.dir/src/Habit.cpp.o"
+  "CMakeFiles/habit_tracker.dir/src/Habit.cpp.o.d"
+  "CMakeFiles/habit_tracker.dir/src/HabitManager.cpp.o"
+  "CMakeFiles/habit_tracker.dir/src/HabitManager.cpp.o.d"
+  "CMakeFiles/habit_tracker.dir/src/main.cpp.o"
+  "CMakeFiles/habit_tracker.dir/src/main.cpp.o.d"
+  "habit_tracker"
+  "habit_tracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/habit_tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
